985ff10c5b52a911b6b2f84628465fecd840ae28
Lorent Elle <lelle@un-i4.kzn.21-school.ru>
2022-05-19 22:30:21 +0300

    reserv

--- test_vector.cpp
+++ test_vector.cpp
@@ -3,57 +3,161 @@
 #include <vector>
 #include <string>
 
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[0;33m"
+#define RED "\033[1;31m"
+#define SHALLOW "\033[0m"
 // #if 1
 // 	namespace ft = std;
 // #endif
 
+// class A 
+// {
+// 	public:
+// 		int _a;
+// 		A(){ std::cout << "constructor called" << std::endl; };
+// 		~A() { std::cout << "destructor called" << std::endl;};
+// 		A(int a):_a(a){}
+		
+
+// };
+
+
+// int main()
+// {
+// 	{
+// 		std::cout << std::endl;
+// 		ft::vector<A> v(5);
+// 		std::cout << std::endl;
+
+// 		// std::cout << std::endl;
+// 		// std::vector<A> v1(5);
+// 		// std::cout << std::endl;
+
+// 	}
+// }
+
+template < class T >
+void print_all(T & my)
+{
+    std::cout << "vector contains:";
+    std::cout << GREEN;
+    for(size_t i = 0; i < my.size(); ++ i)
+        std::cout << " " <<  my[i] ;
+    std::cout<<SHALLOW;
+    std::cout << RED;
+    std::cout << " | | ";
+    for(size_t i = my.size(); i < my.capacity(); ++ i)
+        std::cout << " " <<  my[i] ;
+    std::cout<<SHALLOW;
+    std::cout << std::endl;
+}
+
 int main()
 {
-	//empty vector
+	ft::vector <int> z;
+    for(int i = 0; i < 3; ++i)
+    {
+        std::cout << "capacity: " << z.capacity() << std::endl;
+        std::cout << "size: " << z.size() << std::endl;
+        z.push_back(5);
+        print_all(z);
+        std::cout << "capacity: " << z.capacity() << std::endl;
+        std::cout << "size: " << z.size() << std::endl << std::endl;
+    }
+
+/*	empty vector
 	ft::vector<std::string> myVector;
 	std::allocator<int> alloc;
 	
-	ft::vector<int> myVector2(2, 55);
-	std::vector<int> Vector2(2, 55);
-	
+	ft::vector<std::string> myVector2;
+	std::vector<std::string> Vector2;
 	std::cout<<"size of my Vector2: "<<myVector2.size()<<std::endl;
 	std::cout<<"size of std Vector2: "<<Vector2.size()<<std::endl;
-	// std::cout<<"capacity of my Vector2: "<<myVector2.capacity()<<std::endl;
-	
-	std::cout<<"max size of my Vector2: "<<myVector2.max_size()<<std::endl;
-	std::cout<<"max size of std Vector2: "<<Vector2.max_size()<<std::endl;
-	
-	std::cout<<"std my Vector2 is empty: "<<myVector2.empty()<<std::endl;
-	std::cout<<"std Vector2 is empty: "<<Vector2.empty()<<std::endl;
-	
-	// myVector2.pop_back();
-	// myVector2.pop_back();
-	Vector2.pop_back();
-	Vector2.pop_back();
-
-	std::cout<<"my Vector2 is empty: "<<myVector2.empty()<<std::endl;
-	std::cout<<"std Vector2 is empty: "<<Vector2.empty()<<std::endl;
-
-
-	//√4611686018427387903 = 2147483648
-	ft::vector<std::string> s1;
-	std::vector<std::string> s2;
-	std::cout<<"max size ft::vector string = "<<s1.max_size()<<std::endl;
-	std::cout<<"max size std::vector string = "<<s2.max_size()<<std::endl;
-
-	Vector2.push_back(4);
-	Vector2.push_back(3);
-	std::cout<<"size of std Vector2: "<<Vector2.size()<<std::endl;
+	std::cout<<"capacity of my Vector2: "<<myVector2.capacity()<<std::endl;
 	std::cout<<"capacity of std Vector2: "<<Vector2.capacity()<<std::endl;
-
-	Vector2.resize(1);
-	std::cout<<"size of std Vector2: "<<Vector2.size()<<std::endl;
+	std::cout << std::endl;
+	
+	myVector2.resize(15);
+	Vector2.resize(15);
+	std::cout<<"size resize(0) of my Vector2: "<<myVector2.size()<<std::endl;
+	std::cout<<"size resize(0) of std Vector2: "<<Vector2.size()<<std::endl;
+	std::cout<<"capacity of my Vector2: "<<myVector2.capacity()<<std::endl;
 	std::cout<<"capacity of std Vector2: "<<Vector2.capacity()<<std::endl;
-	std::cout<<"top of std Vector2: "<<Vector2.back()<<std::endl;
-
-	Vector2.resize(11);
-	std::cout<<"size of std Vector2: "<<Vector2.size()<<std::endl;
+	std::cout <<std::endl;
+	
+	// myVector2.reserve(13);
+	// Vector2.reserve(13);
+	std::cout<<"size after reserve(10) of my Vector2: "<<myVector2.size()<<std::endl;
+	std::cout<<"size after reserve(10) of std Vector2: "<<Vector2.size()<<std::endl;
+	std::cout<<"capacity of my Vector2: "<<myVector2.capacity()<<std::endl;
 	std::cout<<"capacity of std Vector2: "<<Vector2.capacity()<<std::endl;
-	std::cout<<"top of std Vector2: "<<Vector2.back()<<std::endl;
+	std::cout << std::endl;
+
+	for (int i = 0; i<11; ++i)
+		myVector2.push_back("sasf");
+	for (int i = 0; i<11; ++i)
+		Vector2.push_back("sasf");
+
+
+	std::cout<<"size after push_back of my Vector2: "<<myVector2.size()<<std::endl;
+	std::cout<<"size after push_back of std Vector2: "<<Vector2.size()<<std::endl;
+	std::cout<<"capacity after push_back of my Vector2: "<<myVector2.capacity()<<std::endl;
+	std::cout<<"capacity after push_back of std Vector2: "<<Vector2.capacity()<<std::endl;
+
+
+
+
+
+	// std::cout<<"max size of my Vector2: "<<myVector2.max_size()<<std::endl;
+	// std::cout<<"max size of std Vector2: "<<Vector2.max_size()<<std::endl;
+	// std::cout << std::endl;
+	
+	// std::cout<<"std my Vector2 is empty: "<<myVector2.empty()<<std::endl;
+	// std::cout<<"std Vector2 is empty: "<<Vector2.empty()<<std::endl;
+	// std::cout << std::endl;
+	
+	// // myVector2.pop_back();
+	// // myVector2.pop_back();
+	// Vector2.pop_back();
+	// Vector2.pop_back();
+	
+
+	// std::cout<<"my Vector2 is empty: "<<myVector2.empty()<<std::endl;
+	// std::cout<<"std Vector2 is empty: "<<Vector2.empty()<<std::endl;
+	// std::cout << std::endl;
+
+
+	// //√4611686018427387903 = 2147483648
+	// ft::vector<std::string> s1;
+	// std::vector<std::string> s2;
+	// std::cout<<"max size ft::vector string = "<<s1.max_size()<<std::endl;
+	// std::cout<<"max size std::vector string = "<<s2.max_size()<<std::endl;
+	// std::cout << std::endl;
+
+	// Vector2.push_back(4);
+	// Vector2.push_back(3);
+	// std::cout<<"size of std Vector2: "<<Vector2.size()<<std::endl;
+	// std::cout<<"capacity of std Vector2: "<<Vector2.capacity()<<std::endl;
+	// std::cout << std::endl;
+
+	// Vector2.resize(4);
+	// std::cout<<"size of std Vector2: "<<Vector2.size()<<std::endl;
+	// std::cout<<"capacity of std Vector2: "<<Vector2.capacity()<<std::endl;
+	// std::cout<<"top of std Vector2: "<<Vector2.back()<<std::endl;
+	// for (int i = 0; i < 4; ++i)
+	// 	std::cout<<Vector2[i]<<std::endl;
+	// std::cout << std::endl;
+
+	// Vector2.resize(11);
+	// std::cout<<"size of std Vector2: "<<Vector2.size()<<std::endl;
+	// std::cout<<"capacity of std Vector2: "<<Vector2.capacity()<<std::endl;
+	// std::cout<<"top of std Vector2: "<<Vector2.back()<<std::endl;
+	// std::cout << std::endl;
+
+	// while (1)
+	// {}
+*/	
 	return (0);
 }	   
+
--- vector.hpp
+++ vector.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <vector>
 #include <iostream>
+#include <unistd.h>
 
 // namespace ft
 // {
@@ -66,7 +67,7 @@ namespace ft
 		public:
 		// (1) empty container constructor (default constructor)
 		explicit vector (const allocator_type& alloc = allocator_type()):
-		_alloc(alloc), _size(0), _p(0)
+		_alloc(alloc), _size(0), _capacity(0), _p(0)
 		{
 			std::cout<<"empty vector constructor called"<<std::endl;
 		};
@@ -74,16 +75,19 @@ namespace ft
 		//(2) fill constructor
 		// Constructs a container with n elements. Each element is a copy of val.
 		explicit vector (size_type _n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()):
-		_alloc(alloc), _size(_n), _capacity(_n)
+		_alloc(alloc), _size(_n)
 		{
-
-			_p = _alloc.allocate(_capacity);
+			_capacity = _size;
+			_p = _alloc.allocate(_capacity);	
 			for (int i = 0; i < _n; ++i)
 			{
-				_alloc.construct(_p + i, val);
+					_alloc.construct(_p + i, val);			
 			}
+
 			std::cout<<"vector with n elements constructor called"<<std::endl;
 		};
+
+
 
 
 		// range constructor
@@ -99,10 +103,8 @@ namespace ft
 			
 		};
 		
-
 		~vector()
 		{
-			
 			if (_p)
 			{
 				for (size_t i = 0; i<_size; i++)
@@ -121,10 +123,27 @@ namespace ft
 			return (allocator_type().max_size());
 		}
 		
-		// void resize (size_type n, value_type val = value_type())
-		// {
-		// 	if ()
-		// }
+		void resize (size_type n, value_type val = value_type())
+		{
+			if (_capacity < n)
+				reserve(n);
+		
+			std::cout<<"pointer in resize "<<_p<<" cap "<< _capacity <<std::endl;
+			for (; _size < n;_size++)
+			{
+				
+				_alloc.construct(_p + _size, val);
+				
+			}
+			
+			for (; _size > n;)
+			{
+				std::cout<<"size "<<_size<<std::endl;
+				this->pop_back();
+			}
+			
+		
+		}
 		size_type capacity() const
 		{
 			return _capacity;
@@ -136,17 +155,67 @@ namespace ft
 			return (_size > 0 ? false : true);
 		}
 
-		void reserve (size_type n);
-	
+		void reserve (size_type n)
+		{
+			if (n > this->max_size())
+				return ;
+			if (n <= _capacity)
+			{
+				
+				return ;
+			}
+
+			pointer _oldp = _p;
+			size_type _oldcap = _capacity;
 		
+
+			_capacity = n;
+
+			_p = _alloc.allocate(_capacity);
+			for (size_type i = 0; i < _size; ++i)
+				_alloc.construct(_p + i, *(_oldp + i));
+	
+			for (size_type i = 0; i < _size; ++i)
+				_alloc.destroy(_oldp + i);
+			_alloc.deallocate(_oldp, _oldcap);
+			
+		}
+	
+
+		void push_back (const value_type& val)
+		{
+			if (_size == _capacity)
+			{
+				if (_size == 0)
+					_capacity = 1;
+				else 
+					_capacity = _capacity * 2;
+				reserve(_capacity);
+			}
+			_alloc.construct(_p + _size, val);
+			++_size;
+		}
+
+		void pop_back()
+		{
+			_size--;
+			_alloc.destroy(_p + _size);
+		}
 
 		// allocator_type get_allocator() const
 		// {
 			
 		// }
 		
+		const_reference operator[] (size_type n) const
+        {
+            return(_p[n]);
+        }
 
-
+		reference operator[] (size_type n) 
+        {
+            return(_p[n]);
+        }
 
 	};
 }

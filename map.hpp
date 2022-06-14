4348901b0e58fc82d1dddbcaad9bc9431b650253
Lorent Elle <lelle@va-a5.kzn.21-school.ru>
2022-06-14 13:06:52 +0300

    map created

--- map.hpp
+++ map.hpp
@@ -4,22 +4,23 @@
 
 #include <iostream> 
 #include <memory> 
+#include <functional>
 #include "utils.hpp"
 #include "iterator.hpp"
  
 namespace ft 
 { 
 
-	template <class _Tp>
-	struct less 
-	{
-		bool operator()(const _Tp& __x, const _Tp& __y) const 
-		{
-			return __x < __y;
-		} 
-	};
+	// template <class _Tp>
+	// struct less 
+	// {
+	// 	bool operator()(const _Tp& __x, const _Tp& __y) const 
+	// 	{
+	// 		return __x < __y;
+	// 	} 
+	// };
 
-	template <class Key, class T, class Compare = less<Key>, class Allocator = std::allocator<pair<const Key, T> > >
+	template <class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<pair<const Key, T> > >
 	class map
 	{
 
@@ -27,15 +28,15 @@ namespace ft
 			// types:
 			typedef Key											key_type;
 			typedef T 											mapped_type;
-			// typedef pair<const key_type, mapped_type> 			value_type;
-			// typedef Compare 									key_compare;
-			// typedef Allocator 									allocator_type;
-			// typedef typename allocator_type::reference 			reference;
-			// typedef typename allocator_type::const_reference	const_reference;
-			// typedef typename allocator_type::pointer 			pointer;
-			// typedef typename allocator_type::const_pointer 		const_pointer;
-			// typedef typename allocator_type::size_type 			size_type;
-			// typedef typename allocator_type::difference_type	difference_type;
+			typedef pair<const key_type, mapped_type> 			value_type;
+			typedef Compare 									key_compare;
+			typedef Allocator 									allocator_type;
+			typedef typename allocator_type::reference 			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer 			pointer;
+			typedef typename allocator_type::const_pointer 		const_pointer;
+			typedef typename allocator_type::size_type 			size_type;
+			typedef typename allocator_type::difference_type	difference_type;
 
 			// typedef implementation-defined 						iterator;
 			// typedef implementation-defined 						const_iterator;
@@ -43,45 +44,38 @@ namespace ft
 			// typedef std::reverse_iterator<const_iterator> 		const_reverse_iterator;
 
 
-			class value_compare : public binary_function<value_type, value_type, bool>
-			{
-				friend class map;
+			// class value_compare : public binary_function<value_type, value_type, bool>
+			// {
+			// 	friend class map;
 
-				protected:
-					key_compare comp;
-					value_compare(key_compare c);
+			// 	protected:
+			// 		key_compare comp;
+			// 		value_compare(key_compare c);
 
-				public:
-					bool operator()(const value_type& x, const value_type& y) const;
+			// 	public:
+			// 		bool operator()(const value_type& x, const value_type& y) const
+			// 			{return comp(__x.first, __y.first);}
+			// };
+
+			// construct:
+			explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()){
+
 			};
 
-			// construct/copy/destroy:
-			map()
-				noexcept(
-					is_nothrow_default_constructible<allocator_type>::value &&
-					is_nothrow_default_constructible<key_compare>::value &&
-					is_nothrow_copy_constructible<key_compare>::value);
-			explicit map(const key_compare& comp);
-			map(const key_compare& comp, const allocator_type& a);
-			template <class InputIterator>
-				map(InputIterator first, InputIterator last, const key_compare& comp = key_compare());
-			
 			// template <class InputIterator>
-			// 	map(InputIterator first, InputIterator last, const key_compare& comp, const allocator_type& a);
-			map(const map& m);
-			map(map&& m)
-				noexcept(is_nothrow_move_constructible<allocator_type>::value &&
-					is_nothrow_move_constructible<key_compare>::value);
-			explicit map(const allocator_type& a);
-			map(const map& m, const allocator_type& a);
-			map(map&& m, const allocator_type& a);
-			map(initializer_list<value_type> il, const key_compare& comp = key_compare());
-			map(initializer_list<value_type> il, const key_compare& comp, const allocator_type& a);
-			template <class InputIterator>
-				map(InputIterator first, InputIterator last, const allocator_type& a)
-					: map(first, last, Compare(), a) {}  // C++14
-			map(initializer_list<value_type> il, const allocator_type& a)
-				: map(il, Compare(), a) {}  // C++14
+			// 	map (InputIterator first, InputIterator last, 
+			// 	const key_compare& comp = key_compare(),
+			// 	const allocator_type& alloc = allocator_type())
+			// {
+				
+			// };
+			map (const map& x){
+
+			};
+
+
+			
+			
 			~map();
 
 			map& operator=(const map& m);
---
+++ tree.hpp
@@ -0,0 +1,24 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+
+//black and red tree
+namespace ft
+{
+	template<class T>
+	struct Node
+	{
+		int color;
+		struct Node *parent;
+		struct Node *left;
+		struct Node *right;
+
+	};
+		
+	class tree
+	{
+
+	}
+
+}
+
+#endif
